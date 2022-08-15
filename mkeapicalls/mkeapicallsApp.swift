37cc82550d777d8db9b44b2782f3a0cfab9867d4
3vilbird <devilisnotinhome@gmail.com>
2022-08-15 12:27:23 +0530

    making api calls with swift

---
+++ mkeapicalls/mkeapicallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mkeapicallsApp.swift
+//  mkeapicalls
+//
+//  Created by admin on 12/08/22.
+//
+
+import SwiftUI
+
+@main
+struct mkeapicallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
